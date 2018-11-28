4ca17ef9d7efbcf20dd884846ecb967ed11ea1fb
pickx <pickyaxe@gmail.com>
2018-11-28 15:41:17 +0200

    first commit

---
+++ src/ExternalMemoryImpl.java
@@ -0,0 +1,36 @@
+package edu.hebrew.db.external;
+
+public class ExternalMemoryImpl implements IExternalMemory {
+
+    @Override
+    public void sort(String in, String out, int colNum, String tmpPath) {
+        // TODO: Implement
+    }
+
+    @Override
+    public void select(String in, String out, int colNumSelect,
+                       String substrSelect, String tmpPath) {
+        // TODO: Implement
+    }
+
+    @Override
+    public void sortAndSelectEfficiently(String in, String out, int colNumSort,
+                                         String tmpPath, int colNumSelect, String substrSelect) {
+        //default values
+        int numColsPerLine = 4;
+        int numCharsPerCol = 20;
+        int colDelimiterLength = 1;
+        int sizeOfChar = 2;
+        int X = sizeOfChar * ( (  numColsPerLine       * numCharsPerCol)     +
+                               ( (numColsPerLine - 1)  * colDelimiterLength) );
+                //max bytes in memory for a line
+        int M = 1000; //block size
+        int Y = 20000; //size of block
+
+
+
+
+
+    }
+
+}
