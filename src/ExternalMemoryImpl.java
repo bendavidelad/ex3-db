89abec8fcc0e7a03d23e955fc0a8175a34aaff89
yuval <pickyaxe@gmail.com>
2018-11-28 15:57:26 +0200

    first commit

--- src/ExternalMemoryImpl.java
+++ src/ExternalMemoryImpl.java
@@ -4,7 +4,17 @@ public class ExternalMemoryImpl implements IExternalMemory {
 
     @Override
     public void sort(String in, String out, int colNum, String tmpPath) {
-        // TODO: Implement
+        //default values
+        int numColsPerLine = 4;
+        int numCharsPerCol = 20;
+        int colDelimiterLength = 1;
+        int sizeOfChar = 2;
+        int X = sizeOfChar * ( (  numColsPerLine       * numCharsPerCol)     +
+                ( (numColsPerLine - 1)  * colDelimiterLength) );
+        //max bytes in memory for a line
+        int M = 1000; //block size
+        int Y = 20000; //size of block
+
     }
 
     @Override
@@ -16,18 +26,6 @@ public class ExternalMemoryImpl implements IExternalMemory {
     @Override
     public void sortAndSelectEfficiently(String in, String out, int colNumSort,
                                          String tmpPath, int colNumSelect, String substrSelect) {
-        //default values
-        int numColsPerLine = 4;
-        int numCharsPerCol = 20;
-        int colDelimiterLength = 1;
-        int sizeOfChar = 2;
-        int X = sizeOfChar * ( (  numColsPerLine       * numCharsPerCol)     +
-                               ( (numColsPerLine - 1)  * colDelimiterLength) );
-                //max bytes in memory for a line
-        int M = 1000; //block size
-        int Y = 20000; //size of block
-
-
 
 
 
